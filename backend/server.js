e0b8d570111430bea021630025b73dfcb2bb3c82
Antriksh <antriksh.scin9@gmail.com>
2023-09-28 13:25:40 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
